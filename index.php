ce93bf65b3c403d12c7afec2b59e82f2f7db4312
Sri karthik <56256718+srikarthikv@users.noreply.github.com>
2023-06-07 10:06:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,62 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>PHP Web App</title>
+    <title>BRT Web app</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #333;
+            font-size: 28px;
+        }
+
+        p {
+            color: #666;
+            font-size: 16px;
+        }
+
+        form {
+            margin-top: 20px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+            color: #333;
+            font-size: 16px;
+        }
+
+        input[type="text"] {
+            padding: 8px;
+            font-size: 14px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            width: 300px;
+            margin-bottom: 10px;
+        }
+
+        button[type="submit"] {
+            padding: 10px 20px;
+            font-size: 16px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 <body>
-    <h1>Hello, PHP!</h1>
-    <p>This is a PHP web app that sends a POST request.</p>
+    <h1>Hello</h1>
+    <p>Welcome to BRTGPT</p>
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
